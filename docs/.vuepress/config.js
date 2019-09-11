d3ac5106aa333449cf563a22fdbc6391158d6974
wubaiqing (琉璃) <wubaiqing@vip.qq.com>
2019-09-11 10:56:02 +0800

    docs: 新增技术栈规范

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,8 +4,9 @@ module.exports = {
   themeConfig: {
     docsDir: "docs",
     sidebar: [
-        ["/", "1. 介绍"],
-        ["/development", "2. 开发工作流规范 (基于 Git)"],
-    ],
+      ["/", "1. 介绍"],
+      ["/development", "2. 开发工作流规范 (基于 Git)"],
+      ["/tech-stack", "3. 技术栈规范"],
+    ]
   }
 };

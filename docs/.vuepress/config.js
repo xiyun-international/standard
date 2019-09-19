f05b8142a6756ba5d028f78e7cca1e6d5c853ec4
wubaiqing (琉璃) <wubaiqing@vip.qq.com>
2019-09-19 16:03:15 +0800

    docs: 新增项目规范-开发优先级

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,8 +9,8 @@ module.exports = {
       ["/development", "2. 开发工作流规范 (基于 Git)"],
       ["/tech-stack", "3. 技术栈规范"],
       ["/project", "4. 项目规范"],
-      ["/environment", "5. 打造舒适的开发环境"],
-      ["/share", "6. 分享计划"],
+      ["/share", "5. 分享计划"],
+      ["/environment", "6. 打造舒适的开发环境"],
     ]
   }
 };
